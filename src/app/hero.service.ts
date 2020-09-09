a7dad413870c59bcbe57ea63abfcf490c4ea1981
Roger Cheng <roger.random@outlook.com>
2020-09-09 16:50:14 -0700

    Update heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,6 +14,9 @@ import { MessageService } from './message.service';
 export class HeroService {
 
   private readonly heroesUrl = 'api/heroes';  // URL to web api
+  private readonly httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(
     private http: HttpClient,
@@ -38,6 +41,14 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
 
