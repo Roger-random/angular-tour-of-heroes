42edffa032d87cdbb2b658da3bb87362a5939d47
Regorlas <roger.random@outlook.com>
2023-02-26 21:58:40 +0000

    Hero search to mat-autocomplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -38,8 +40,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     ),
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatAutocompleteModule,
     MatButtonModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
     MatToolbarModule,
   ],
