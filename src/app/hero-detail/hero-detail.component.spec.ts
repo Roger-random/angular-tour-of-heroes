07c8c9f9ebb0a35001089a4af3f2e3f48c2a3032
Regorlas <roger.random@outlook.com>
2023-03-14 01:18:13 +0000

    Verify HTTP hero fetch

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,16 +1,24 @@
-import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// Angular Test Support
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RouterTestingModule } from "@angular/router/testing";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// Application objects under test
 import { HeroDetailComponent } from './hero-detail.component';
+import { Hero } from '../hero';
 
 describe('HeroDetailComponent', () => {
+  const placeholderHero: Hero =
+    { id: 1, name: 'A' };
+
   let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        HttpClientModule,
+        HttpClientTestingModule,
         RouterTestingModule,
       ],
       declarations: [
@@ -22,6 +30,16 @@ describe('HeroDetailComponent', () => {
     fixture = TestBed.createComponent(HeroDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+    const req = httpTestingController.expectOne('api/heroes/0');
+    expect(req.request.method).withContext('HTTP method').toEqual('GET');
+    req.flush(placeholderHero);
+  });
+
+  afterEach(() => {
+    // Assert there are no more pending requests
+    httpTestingController.verify();
   });
 
   it('should create', () => {
