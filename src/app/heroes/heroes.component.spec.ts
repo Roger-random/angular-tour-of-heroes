9fd23224953dbbd26d855f76fbc942cf345c5cbd
Regorlas <roger.random@outlook.com>
2023-03-14 19:21:46 +0000

    Improve code coverage

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,9 +1,40 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
+import { Observable, of } from 'rxjs';
+
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
+import { AppRoutingModule } from '../app-routing.module';
+
 import { HeroesComponent } from './heroes.component';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+
+const mockHeroes: Hero[] = [{ id: 1, name: 'Zoq' }, { id: 2, name: 'Fot' }];
+const newHero: Hero = {id: 3, name: 'Pik'};
+
+class MockHeroService {
+  heroList: Hero[] = mockHeroes;
+
+  getHeroes(): Observable<Hero[]> {
+    return of(this.heroList);
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    if(hero.name !== newHero.name) {
+      throw new Error(`new hero name ${hero.name} did not match expected name ${newHero.name}`);
+    }
+    this.heroList.push(hero);
+    return of(hero);
+  }
+
+  deleteHero(hero: Hero): Observable<Hero> {
+    this.heroList = this.heroList.filter(h => h !== hero);
+    return of(hero);
+  }
+}
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
@@ -12,13 +43,23 @@ describe('HeroesComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
+        AppRoutingModule,
         HttpClientTestingModule,
+        MatIconModule,
         MatListModule
       ],
       declarations: [
         HeroesComponent,
       ],
     })
+    .overrideComponent(
+      HeroesComponent,
+      {set:
+        {providers: [
+          {provide: HeroService, useClass: MockHeroService}
+        ]}
+      }
+    )
     .compileComponents();
 
     fixture = TestBed.createComponent(HeroesComponent);
@@ -28,5 +69,27 @@ describe('HeroesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should retrieve hero list in ngOnInit', () => {
+    expect(component.heroes).withContext('retrieved mock hero list').toEqual(mockHeroes);
+  });
+
+  it('adding empty name should not change hero list', () => {
+    component.add('');
+    expect(component.heroes).withContext('unchanged mock hero list').toEqual(mockHeroes);
+  });
+
+  it('adding name should add to hero list', () => {
+    var heroList: Hero[] = mockHeroes;
+    component.add(newHero.name);
+    heroList.push(newHero);
+    expect(component.heroes).withContext('hero list with added hero').toEqual(heroList);
+  });
+
+  it('deleting hero should shorten hero list', () => {
+    var heroList: Hero[] = mockHeroes.filter(h => h !== mockHeroes[0]);
+    component.delete(mockHeroes[0]);
+    expect(component.heroes).withContext('hero list with hero removed').toEqual(heroList);
   });
 });
