5e81a3de070947ba137ced643e603b37a3c01738
Regorlas <roger.random@outlook.com>
2023-03-14 22:14:03 +0000

    Incomplete test for hero search

--- src/app/hero-search/hero-search.component.spec.ts
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -1,5 +1,5 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 import { HeroSearchComponent } from './hero-search.component';
+import { Hero } from '../hero';
 
 describe('HeroSearchComponent', () => {
   let component: HeroSearchComponent;
@@ -35,4 +36,26 @@ describe('HeroSearchComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('search returns empty Heroes', fakeAsync((done: DoneFn) => {
+    let componentHeroes: Hero[] = [];
+
+    component.heroes$.subscribe({
+      next: heroes => {
+        // TODO: This never gets called?
+        componentHeroes = heroes;
+        done()
+      },
+      error: error => {
+        done.fail;
+      }
+    });
+    component.search('m');
+    tick(100);
+    component.search('ma');
+    tick(500);
+    fixture.whenStable().then(() => {
+      expect(componentHeroes).toEqual([]);
+    });
+  }));
 });
