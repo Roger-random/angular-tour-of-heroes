43f501ce4c4c797e39761b6df38fcae9f1da3c46
Regorlas <roger.random@outlook.com>
2023-03-15 02:15:36 +0000

    Fix test for hero search

--- src/app/hero-search/hero-search.component.spec.ts
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -1,13 +1,29 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
+import { Observable, of } from 'rxjs';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 import { HeroSearchComponent } from './hero-search.component';
+import { HeroesComponent } from '../heroes/heroes.component';
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
+
+const searchTerm: string = 'man';
+const batSuperMan: Hero[] = [{ id: 1, name: 'Batman' }, { id: 2, name: 'Superman' }];
+
+class MockHeroService {
+  searchHeroes(name: string): Observable<Hero[]> {
+    if (name !== searchTerm) {
+      throw new Error(`Expected search term ${searchTerm} but received ${name}`);
+    }
+    return of(batSuperMan);
+  }
+}
 
 describe('HeroSearchComponent', () => {
   let component: HeroSearchComponent;
@@ -26,6 +42,14 @@ describe('HeroSearchComponent', () => {
         HeroSearchComponent,
       ],
     })
+    .overrideComponent(
+      HeroSearchComponent,
+      {set:
+        {providers: [
+          {provide: HeroService, useClass: MockHeroService}
+        ]}
+      }
+    )
     .compileComponents();
 
     fixture = TestBed.createComponent(HeroSearchComponent);
@@ -42,20 +66,21 @@ describe('HeroSearchComponent', () => {
 
     component.heroes$.subscribe({
       next: heroes => {
-        // TODO: This never gets called?
         componentHeroes = heroes;
-        done()
+        done;
       },
       error: error => {
         done.fail;
       }
     });
-    component.search('m');
+    component.search('m'); // Should never reach MockHeroService due to debounceTime()
     tick(100);
-    component.search('ma');
+    component.search('ma');  // Should never reach MockHeroService due to debounceTime()
+    tick(100);
+    component.search('man');
     tick(500);
     fixture.whenStable().then(() => {
-      expect(componentHeroes).toEqual([]);
+      expect(componentHeroes).toEqual(batSuperMan);
     });
   }));
 });
